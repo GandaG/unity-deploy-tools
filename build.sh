e16e87c65c1d6753c06a9b4730ce405a9daa2485
GandaG <GandaG@users.noreply.github.com>
2016-01-11 20:21:05 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,31 @@
 
 project="ci-build"
 
-echo "Attempting to package $project"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $(pwd)/unity.log -exportPackage $(pwd) $(project) -quit
+echo "Setting up project directory:"
+mkdir $(pwd)/Project
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+-batchmode \
+-nographics \
+-silent-crashes \
+-logFile $(pwd)/unityProject.log \
+-createProject $(pwd)/Project \
+-quit
+
+echo "Move files to Assets/"
+mkdir $(pwd)/Project/Assets/$(project)
+mv $(pwd)/* $(pwd)/Project/Assets/$(project)
+rm -r $(pwd)/Project/Assets/$(project)/Project
+
+echo "Attempting to package $project:"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+-batchmode \
+-nographics \
+-silent-crashes \
+-logFile $(pwd)/unityPackage.log \
+-projectPath $(pwd)/Project \
+-exportPackage $(pwd)/Project/Assets/$(project) $(project) \
+-quit
 
 echo 'Logs from package:'
-cat $(pwd)/unity.log
+cat $(pwd)/unityProject.log
+cat $(pwd)/unityPackage.log
