774fd0e22c0709c52aca27f9ff1d230d19b0be1b
GandaG <GandaG@users.noreply.github.com>
2016-01-11 05:23:05 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,14 +3,7 @@
 project="ci-build"
 
 echo "Attempting to package $project"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath=$(pwd) \
-  -exportPackage $(pwd) $(project) \
-  -quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $(pwd)/unity.log -exportPackage $(pwd) $(project) -quit
 
-echo 'Logs from package'
+echo 'Logs from package:'
 cat $(pwd)/unity.log
