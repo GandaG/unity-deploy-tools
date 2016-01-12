849fb3fc1b8d7ee9467f95ba356b3972f9283c8f
GandaG <GandaG@users.noreply.github.com>
2016-01-12 02:08:23 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,35 +3,29 @@
 project="ci-build"
 
 echo "Setting up project directory:"
-mkdir $(pwd)/Project
+mkdir "$(pwd)"/Project
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
 -batchmode \
 -nographics \
 -silent-crashes \
--logFile $(pwd)/unityProject.log \
--createProject $(pwd)/Project \
+-logFile "$(pwd)"/unityProject.log \
+-createProject "$(pwd)"/Project \
 -quit
 
 echo "Move files to Assets/"
-mkdir $(pwd)/Project/Assets/$project
-for file in $(pwd)/*
-do
-  if [ $file != Project -o $file != install.sh -o $file != Unity.pkg -o $file != unityProject.log -o $file != build.sh ]
-  then
-    mv $file $(pwd)/Project/Assets/$project/
-  fi
-done
+mkdir -p "$(pwd)"/Project/Assets/$project
+find . -path ./Project/ -prune -o \! -name "*.sh" \! -name "*.pkg" \! -name "*.log" -name "*" -exec mv {} "$(pwd)"/Project/Assets/$project/ \;
 
 echo "Attempting to package $project:"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
 -batchmode \
 -nographics \
 -silent-crashes \
--logFile $(pwd)/unityPackage.log \
--projectPath $(pwd)/Project \
--exportPackage $(pwd)/Project/Assets/$project $project \
+-logFile "$(pwd)"/unityPackage.log \
+-projectPath "$(pwd)"/Project \
+-exportPackage "$(pwd)"/Project/Assets/$project $project \
 -quit
 
 echo 'Logs from package:'
-cat $(pwd)/unityProject.log
-cat $(pwd)/unityPackage.log
+cat "$(pwd)"/unityProject.log
+cat "$(pwd)"/unityPackage.log
