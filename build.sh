4c0eafd8a2d9c2c28473f4b33d68448280aee911
GandaG <GandaG@users.noreply.github.com>
2016-01-13 00:03:06 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,47 +3,43 @@
 project="ci-build"
 
 echo "Setting up project directory;"
-echo ~/
-mkdir "$(pwd)"/Project
-pwd
+mkdir ./Project
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
  -batchmode \
  -nographics \
  -silent-crashes \
- -logFile "$(pwd)"/unityProject.log \
- -createProject "$(pwd)"/Project \
+ -logFile ./unityProject.log \
+ -createProject ./Project \
  -quit
 
 echo "Moving files into temporary project;"
-pwd
-mkdir -p "$(pwd)"/Project/Assets/$project
-pwd
+mkdir -p ./Project/Assets/$project
 #find "$(pwd)" \( -type d \( -path "$(pwd)"/Project/ -o -path "$(pwd)"/.git/ \) \
 # -o -type f \( -name "*.sh" -o -name "*.pkg" -o -name "*.log" -o -name ".gitignore" \) \) \
 # -prune -o \
 # \( -type f -o -type d \) \
 # -exec mv {} "$(pwd)"/Project/Assets/$project/ \;
  
-find "$(pwd)" \
- ! -path "$(pwd)"/Project/ \
- ! -path "$(pwd)"/.git/ \
+find . \
+ ! -path ./Project/ \
+ ! -path ./.git/ \
  ! -name "*.sh" \
  ! -name "*.pkg" \
  ! -name "*.log" \
  ! -name ".gitignore" \
  -type f \
- -exec mv {} "$(pwd)"/Project/Assets/$project/ \;
+ -exec mv {} ./Project/Assets/$project/ \;
 
 echo "Attempting to package $project;"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
  -batchmode \
  -nographics \
  -silent-crashes \
- -logFile "$(pwd)"/unityPackage.log \
- -projectPath "$(pwd)"/Project \
- -exportPackage "$(pwd)"/Project/Assets/$project $project \
+ -logFile ./unityPackage.log \
+ -projectPath ./Project \
+ -exportPackage ./Project/Assets/$project $project \
  -quit
 
 echo 'Logs from package:'
-cat "$(pwd)"/unityProject.log
-cat "$(pwd)"/unityPackage.log
+cat ./unityProject.log
+cat ./unityPackage.log
