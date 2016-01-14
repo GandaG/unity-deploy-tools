bbc2fae9a517795bc1c0fa4dc4132253a44e2f17
Daniel Nunes <gandaganza@gmail.com>
2016-01-14 00:23:14 +0000

    Moved variables in yml to global. Testing to see if they actually work.

--- CI/install.sh
+++ CI/install.sh
@@ -1,5 +1,10 @@
 #! /bin/sh
 
+#testing variables
+find ./
+echo $project
+echo $package
+
 echo 'Downloading from http://download.unity3d.com/download_unity/f3d16a1fa2dd/MacEditorInstaller/Unity-5.2.3f1.pkg: '
 curl -o CI/Unity.pkg http://download.unity3d.com/download_unity/f3d16a1fa2dd/MacEditorInstaller/Unity-5.2.3f1.pkg
 
