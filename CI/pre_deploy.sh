a61145dc8940a406b72f91c4b689797b7a03a69a
Daniel Nunes <gandaganza@gmail.com>
2016-01-15 03:18:06 +0000

    Testing deployment support. No deployment testing first, if successful tagged deployment will be tested next. Added py file to setup post requests if needed, pre_deploy should now output the correct yml file. When deploying, shell files should only run on rebuild.

--- CI/pre_deploy.sh
+++ CI/pre_deploy.sh
@@ -1,21 +1,25 @@
 #! /bin/sh
 
+project="${TRAVIS_REPO_SLUG##*/}"
+package=$project.unitypackage
 
-if [ $project == "unitypackage-ci" ];
+if [ "$project" == "unitypackage-ci" ];
 then
    mkdir ./Temp
    mkdir ./Deploy
    
+   #grab everything inside CI/ except the files created during the build.
    find ./CI/ \ 
     ! -path '*/\.*' \ 
 	! -name "*.pkg" \ 
 	! -name "*.log" \ 
 	! -name ".gitignore" \ 
 	-exec mv {} ./Temp/ \; 
-	
+
+   #also grab the readme and the license.
    find ./* \
-    -name "README.rst"
-	-name "LICENSE"
+    -name "README.rst" \
+	-name "LICENSE" \
 	-exec mv {} ./Temp/ \;
 	
    #checking the files inside temp - for testing only
@@ -25,34 +29,28 @@ then
    /bin/cat <<EOM > ./Temp/.travis.yml
    language: objective-c
 
-   install:
-    - sh CI/install.sh
-   
-   script:
-    - sh CI/build.sh
-    
-   before_deploy:
-    - sh CI/pre_deploy.sh
-   
-   deploy:
-     provider: releases
-     api_key: 
-      secure: $GH_TOKEN_SECURE
-     file: ./Deploy/$project.zip
-     skip_cleanup: true
-     on:
-       tags: true
-   env:
-    - project = "${TRAVIS_REPO_SLUG##*/}"
-    - package = $project.unitypackage
-    - GH_TOKEN_SECURE = put.encrypted.oauth.gh.token.here
+before_install:
+  - pip install requests
+  - python ./CI/set_up.py
+
+install:
+  - sh CI/install.sh
+
+script:
+  - sh CI/build.sh
+  
+env:
+  global:
+    - secure: API_encrypted_token_here
+    - secure: Gihutb_encrypted_token_here
+
 EOM
    
-   zip ./Temp/* ./Deploy/$project.zip
+   zip ./Temp/* "./Deploy/$project.zip"
 else
    mkdir ./Deploy
    
    find ./Project/* \
-    -name $package
-	-exec zip {} ./Deploy/$project.zip
+    -name "$package" \
+	-exec zip {} "./Deploy/$project.zip" \;
 fi
---
+++ CI/set_up.py
@@ -0,0 +1,75 @@
+import requests
+
+try:
+	API_TOKEN #check if there is an api token. If not, skip rebuild and deployment.
+except NameError:
+	exit(0)
+
+if !TRAVIS_PULL_REQUEST:
+	exit(0) #It's better to not rebuild on pr since the secure variables are not shared.
+
+project = TRAVIS_REPO_SLUG.split("/")[1]
+
+package = "%s.unitypackage" % project
+
+url = "https://api.travis-ci.org/repo/%s/requests" % TRAVIS_REPO_SLUG
+
+branch = TRAVIS_BRANCH
+
+headers = {"Content-Type": "application/json",
+			"Accept": "application/json",
+			"Travis-API-Version": "3",
+			"Authorization": API_TOKEN}
+
+baseymldict = {"language": ["objective-c"],
+				"before_install": ["sh CI/install.sh"],
+				"script": ["sh CI/build.sh"],
+				"before_deploy": ["sh CI/pre_deploy.sh"],
+				"env": {"global": ["REBUILDING = true"]}
+				}
+
+requestdict = {"message": "Testing API requests. Rebuilding with different yml file.",
+				"branch": branch,
+				"config": baseymldict}
+
+json = {"request": requestdict}
+
+try:
+	GH_TOKEN
+except NameError:
+	print "Not deploying to Github Releases."
+else:
+	deploy_gh = {
+		"deploy": [
+			{
+			"provider": "releases",
+			"api_key": [
+				{
+				"secure": GH_TOKEN
+				}
+			],
+			"file": "./Deploy/%s.zip" % project,
+			"skip_cleanup": "true",
+			"on": {
+				"tags": "true"
+			}
+			}
+		]
+		}
+	baseymldict["deploy"] = deploy_gh
+
+response = requests.post(url, headers=headers, json=json)
+
+if response.status_code != requests.codes.ok:
+	print "Post request failed, retrying..."
+	r2 = requests.post(url, headers=headers, json=json)
+	if r2.status_code != requests.codes.ok:
+		raise r2.raise_for_status()
+		exit(1)
+	else:
+		print r2.content
+else:
+	print response.content
+
+
+
