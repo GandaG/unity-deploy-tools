91f80885ff2cf5ae60fb74247a790c270e267770
Daniel Nunes <gandaganza@gmail.com>
2016-01-16 02:09:41 +0000

    Fixed variable check at setup.

--- CI/set_up.py
+++ CI/set_up.py
@@ -1,7 +1,7 @@
 import requests
 
 try:
-	API_TOKEN #check if there is an api token. If not, skip rebuild and deployment.
+	$API_TOKEN #check if there is an api token. If not, skip rebuild and deployment.
 except NameError:
 	print "API token not found. Skipping deployment."
 	exit(0)
