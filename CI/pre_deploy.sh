ce5aaad2c019b0ef5797324a0129c7f5318f7700
Daniel Nunes <gandaganza@gmail.com>
2016-01-19 01:14:55 +0000

    Testing whitespace.

--- CI/pre_deploy.sh
+++ CI/pre_deploy.sh
@@ -3,31 +3,25 @@
 project="${TRAVIS_REPO_SLUG##*/}"
 package=$project.unitypackage
 
-if [ "$project" == "unitypackage-ci" ];
-then
-    echo "Preparing directories;"
-    mkdir ./Temp
-    mkdir ./Deploy
-    
-    #grab everything inside CI/ except the files created during the build.
-    echo "Move stuff inside CI/ to Temp/"
-    mv ./CI ./Temp/CI
-    
-    find ./Temp/CI/* \
-     -path "*/\.*" \
-     -name "*.pkg" \
-     -name "*.log" \
-     -name ".gitignore"
-    
-    echo "Grab the README and the LICENSE."
-    #also grab the readme and the license.
-    find ./* \
-     -name "README.rst" \
-     -name "LICENSE" \
-     -exec mv {} ./Temp/ \;
-    
-    echo "language: objective-c
-
+if [ "$project" == "unitypackage-ci" ]; then
+  echo "Preparing directories;"
+  mkdir ./Temp
+  mkdir ./Deploy
+  
+  #grab everything inside CI/ except the files created during the build.
+  echo "Move stuff inside CI/ to Temp/"
+  mv ./CI ./Temp/CI
+  
+  find ./Temp/CI/* -path "*/\.*" -name "*.pkg" -name "*.log" -name ".gitignore"
+  
+  echo "Grab the README and the LICENSE."
+  #also grab the readme and the license.
+  find ./* \
+   -name "README.rst" \
+   -name "LICENSE" \
+   -exec mv {} ./Temp/ \;
+  
+  echo "language: objective-c
 install:
   - sh ./CI/py_set_up.sh
   - python ./CI/deploy_set_up.py
@@ -39,20 +33,20 @@ script:
 env:
     global:
       - secure: Github_encrypted_token_here" > ./Temp/.travis.yml
-    
-    #checking the files inside temp - for testing only
-    echo "All files inside temp;"
-    find ./Temp/*
-    
-    echo "New yml file:"
-    cat ./Temp/.travis.yml
-    
-    echo "Zip everything up."
-    zip ./Temp/* "./Deploy/$project.zip"
+  
+  #checking the files inside temp - for testing only
+  echo "All files inside temp;"
+  find ./Temp/*
+  
+  echo "New yml file:"
+  cat ./Temp/.travis.yml
+  
+  echo "Zip everything up."
+  zip ./Temp/* "./Deploy/$project.zip"
 else
-    mkdir ./Deploy
-    
-    find ./Project/* \
-     -name "$package" \
-     -exec zip {} "./Deploy/$project.zip" \;
+  mkdir ./Deploy
+  
+  find ./Project/* \
+   -name "$package" \
+   -exec zip {} "./Deploy/$project.zip" \;
 fi
