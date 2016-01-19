1b017e43b542d362c30bbb54abd2bb4538211253
Daniel Nunes <gandaganza@gmail.com>
2016-01-19 21:57:35 +0000

    Debugging.

--- CI/pre_deploy.sh
+++ CI/pre_deploy.sh
@@ -8,6 +8,10 @@ if [ "$project" == "unitypackage-ci" ]; then
   mkdir ./Temp
   mkdir ./Deploy
   
+  #Debugging.
+  echo "Everything inside ."
+  find ./*
+  
   #grab everything inside CI/ except the files created during the build.
   echo "Move stuff inside CI/ to Temp/"
   mv ./CI ./Temp/CI
@@ -16,8 +20,8 @@ if [ "$project" == "unitypackage-ci" ]; then
   
   echo "Grab the README and the LICENSE."
   #also grab the readme and the license.
-  mv './README.rst' './Temp/README.rst'
-  mv './LICENSE' './Temp/LICENSE'
+  mv "$PWD"/README.rst "$PWD"/Temp/README.rst
+  mv "$PWD"/LICENSE "$PWD"/Temp/LICENSE
   
   echo "language: objective-c
 
